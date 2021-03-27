f3349f15d81b0f316d3408f75fc635acdaabd06d
Matt Filetto <matt.filetto@gmail.com>
2021-03-27 00:37:51 -0700

    fs: crypto, ext4, f2fs: discard Samsung changes except HPB support
    Partially reverts "sm8250: Import G9810ZCU1ATD1 from OSRC"
    
    This change allows us to merge upstream FS changes easier.
    Without vendor specific changes, the FS behavior will be
    consistent with generic. That might allow greater stability and
    performance on third party ROMs which are not based on stock.
    Plus, hopefully we can also see some performance improvements
    without heavy security additions like SDP and DDAR.
    
    Note that this is a breaking deviation of lineage-17.1 from
    stock-10. It is known that Kernel is NOT bootable on stock
    firmware without certain changes, SDP and DDAR specifically.
    
    Change-Id: Id1cbdcb0c0dac490636620a69ee1c609bbb0dd1c
    Signed-off-by: Jesse Chan <jc@linux.com>
    
    # Conflicts:
    #	fs/crypto/Kconfig
    #	fs/crypto/Makefile
    #	fs/crypto/crypto.c
    #	fs/crypto/fscrypt_ddar.c
    #	fs/crypto/fscrypt_knox_private.h
    #	fs/crypto/fscrypt_private.h
    #	fs/crypto/policy.c
    #	fs/crypto/sdp/fscrypto_sdp_dek_private.h
    #	fs/crypto/sdp/fscrypto_sdp_private.h
    #	fs/crypto/sdp/fscrypto_sdp_xattr_private.h
    #	fs/crypto/sdp/sdp_cache.c
    #	fs/crypto/sdp/sdp_crypto.c
    #	fs/crypto/sdp/sdp_crypto.h
    #	fs/crypto/sdp/sdp_dek.c
    #	fs/crypto/sdp/sdp_ioctl.c
    #	fs/crypto/sdp/sdp_name.c
    #	fs/crypto/sdp/sdp_xattr.c
    #	fs/ext4/dir.c
    #	fs/ext4/ext4.h
    #	fs/ext4/ext4_jbd2.c
    #	fs/ext4/extents.c
    #	fs/ext4/inode.c
    #	fs/ext4/ioctl.c
    #	fs/ext4/namei.c
    #	fs/ext4/super.c
    #	fs/ext4/xattr.c
    #	fs/f2fs/Kconfig
    #	fs/f2fs/checkpoint.c
    #	fs/f2fs/data.c
    #	fs/f2fs/debug.c
    #	fs/f2fs/f2fs.h
    #	fs/f2fs/file.c
    #	fs/f2fs/gc.c
    #	fs/f2fs/segment.c
    #	fs/f2fs/segment.h
    #	fs/f2fs/super.c
    #	fs/f2fs/sysfs.c
    #	include/linux/f2fs_fs.h
    #	include/linux/fscrypt.h
    #	include/uapi/linux/fs.h

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -415,9 +415,6 @@ static int __check_block_validity(struct inode *inode, const char *func,
 		return 0;
 	if (!ext4_data_block_valid(EXT4_SB(inode->i_sb), map->m_pblk,
 				   map->m_len)) {
-		printk(KERN_ERR "printing inode..\n");
-		print_block_data(inode->i_sb, 0, (unsigned char *)inode, 0,
-				EXT4_INODE_SIZE(inode->i_sb));
 		ext4_error_inode(inode, func, line, map->m_pblk,
 				 "lblock %lu mapped to illegal pblock %llu "
 				 "(length %d)", (unsigned long) map->m_lblk,
@@ -1187,9 +1184,6 @@ static int ext4_block_write_begin(struct page *page, loff_t pos, unsigned len,
 	unsigned bbits;
 	struct buffer_head *bh, *head, *wait[2], **wait_bh = wait;
 	bool decrypt = false;
-#ifdef CONFIG_DDAR
-	bool dd_decrypt = false;
-#endif
 
 	BUG_ON(!PageLocked(page));
 	BUG_ON(from > PAGE_SIZE);
@@ -1244,10 +1238,6 @@ static int ext4_block_write_begin(struct page *page, loff_t pos, unsigned len,
 			ll_rw_block(REQ_OP_READ, 0, 1, &bh);
 			*wait_bh++ = bh;
 			decrypt = fscrypt_inode_uses_fs_layer_crypto(inode);
-
-#ifdef CONFIG_DDAR
-			dd_decrypt = fscrypt_dd_encrypted_inode(inode);
-#endif
 		}
 	}
 	/*
@@ -1262,12 +1252,6 @@ static int ext4_block_write_begin(struct page *page, loff_t pos, unsigned len,
 		page_zero_new_buffers(page, from, to);
 	else if (decrypt)
 		err = fscrypt_decrypt_pagecache_blocks(page, PAGE_SIZE, 0);
-
-#ifdef CONFIG_DDAR
-	if (dd_decrypt)
-		err = fscrypt_dd_decrypt_page(inode, page);
-#endif
-
 	return err;
 }
 #endif
@@ -2229,8 +2213,6 @@ static int ext4_writepage(struct page *page,
 		return -ENOMEM;
 	}
 	ret = ext4_bio_write_page(&io_submit, page, len, wbc, keep_towrite);
-
-	if (ext4_io_submit_to_dd(inode, &io_submit) == -EOPNOTSUPP)
 		ext4_io_submit(&io_submit);
 	/* Drop io_end reference we got from init */
 	ext4_put_io_end_defer(io_submit.io_end);
@@ -2870,9 +2852,7 @@ retry:
 	}
 	ret = mpage_prepare_extent_to_map(&mpd);
 	/* Submit prepared bio */
-	if (ext4_io_submit_to_dd(inode, &mpd.io_submit) == -EOPNOTSUPP)
 		ext4_io_submit(&mpd.io_submit);
-
 	ext4_put_io_end_defer(mpd.io_submit.io_end);
 	mpd.io_submit.io_end = NULL;
 	/* Unlock pages we didn't use */
@@ -2945,9 +2925,7 @@ retry:
 			mpd.do_map = 0;
 		}
 		/* Submit prepared bio */
-		if (ext4_io_submit_to_dd(inode, &mpd.io_submit) == -EOPNOTSUPP)
 			ext4_io_submit(&mpd.io_submit);
-
 		/* Unlock pages we didn't use */
 		mpage_release_unused_pages(&mpd, give_up_on_write);
 		/*
@@ -4174,10 +4152,6 @@ static int __ext4_block_zero_page_range(handle_t *handle,
 			BUG_ON(blocksize != PAGE_SIZE);
 			WARN_ON_ONCE(fscrypt_decrypt_pagecache_blocks(
 						page, PAGE_SIZE, 0));
-#ifdef CONFIG_DDAR
-			if (fscrypt_dd_encrypted_inode(inode))
-				WARN_ON_ONCE(fscrypt_dd_decrypt_page(page->mapping->host, page));
-#endif
 		}
 	}
 	if (ext4_should_journal_data(inode)) {
@@ -4990,7 +4964,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 	raw_inode = ext4_raw_inode(&iloc);
 
 	if ((ino == EXT4_ROOT_INO) && (raw_inode->i_links_count == 0)) {
-		print_iloc_info(sb, iloc);
 		ext4_error_inode(inode, function, line, 0,
 				 "iget: root inode unallocated");
 		ret = -EFSCORRUPTED;
@@ -5008,7 +4981,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 		if (EXT4_GOOD_OLD_INODE_SIZE + ei->i_extra_isize >
 			EXT4_INODE_SIZE(inode->i_sb) ||
 		    (ei->i_extra_isize & 3)) {
-			print_iloc_info(sb, iloc);
 			ext4_error_inode(inode, function, line, 0,
 					 "iget: bad extra_isize %u "
 					 "(inode size %u)",
@@ -5033,7 +5005,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 	}
 
 	if (!ext4_inode_csum_verify(inode, raw_inode, ei)) {
-		print_iloc_info(sb, iloc);
 		ext4_error_inode(inode, function, line, 0,
 				 "iget: checksum invalid");
 		ret = -EFSBADCRC;
@@ -5074,7 +5045,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 		    ino != EXT4_BOOT_LOADER_INO) {
 			/* this inode is deleted */
 			ret = -ESTALE;
-			print_iloc_info(sb, iloc);
 			goto bad_inode;
 		}
 		/* The only unlinked inodes we let through here have
@@ -5093,7 +5063,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 			((__u64)le16_to_cpu(raw_inode->i_file_acl_high)) << 32;
 	inode->i_size = ext4_isize(sb, raw_inode);
 	if ((size = i_size_read(inode)) < 0) {
-		print_iloc_info(sb, iloc);
 		ext4_error_inode(inode, function, line, 0,
 				 "iget: bad i_size value: %lld", size);
 		ret = -EFSCORRUPTED;
@@ -5183,7 +5152,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 	ret = 0;
 	if (ei->i_file_acl &&
 	    !ext4_data_block_valid(EXT4_SB(sb), ei->i_file_acl, 1)) {
-		print_iloc_info(sb, iloc);
 		ext4_error_inode(inode, function, line, 0,
 				 "iget: bad extended attribute block %llu",
 				 ei->i_file_acl);
@@ -5200,10 +5168,8 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 				ret = ext4_ind_check_inode(inode);
 		}
 	}
-	if (ret) {
-		print_iloc_info(sb, iloc);
+	if (ret)
 		goto bad_inode;
-	}
 
 	if (S_ISREG(inode->i_mode)) {
 		inode->i_op = &ext4_file_inode_operations;
@@ -5215,7 +5181,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 	} else if (S_ISLNK(inode->i_mode)) {
 		/* VFS does not allow setting these so must be corruption */
 		if (IS_APPEND(inode) || IS_IMMUTABLE(inode)) {
-			print_iloc_info(sb, iloc);
 			ext4_error_inode(inode, function, line, 0,
 					 "iget: immutable or append flags "
 					 "not allowed on symlinks");
@@ -5248,7 +5213,6 @@ struct inode *__ext4_iget(struct super_block *sb, unsigned long ino,
 		make_bad_inode(inode);
 	} else {
 		ret = -EFSCORRUPTED;
-		print_iloc_info(sb, iloc);
 		ext4_error_inode(inode, function, line, 0,
 				 "iget: bogus i_mode (%o)", inode->i_mode);
 		goto bad_inode;
